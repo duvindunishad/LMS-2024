410cd7895cd4c45343d92cbd47cf5f38fe7cdeb7
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-07-26 16:08:12 +0530

    create the slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,5 +1,32 @@
-const Slider = () => {
-  return <div>Slider</div>;
+// src/CategorySlider.js
+import './slider.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee, faAppleAlt, faCarrot, faFish, faDrumstickBite, faCheese, faPizzaSlice, faIceCream } from '@fortawesome/free-solid-svg-icons';
+
+const categories = [
+  { name: 'Category 1', icon: faCoffee },
+  { name: 'Category 2', icon: faAppleAlt },
+  { name: 'Category 3', icon: faCarrot },
+  { name: 'Category 4', icon: faFish },
+  { name: 'Category 5', icon: faDrumstickBite },
+  { name: 'Category 6', icon: faCheese },
+  { name: 'Category 7', icon: faPizzaSlice },
+  { name: 'Category 8', icon: faIceCream }
+];
+
+const slider = () => {
+  return (
+    <div className="slider-container">
+      <div className="slider">
+        {categories.map((category, index) => (
+          <div key={index} className="slider-item">
+            <FontAwesomeIcon icon={category.icon} size="2x" />
+            <p>{category.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Slider;
+export default slider;
