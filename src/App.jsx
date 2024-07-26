018e59ca970a7702f806f0cb84e0fcae2c7eb92f
AvishkaGihan <avishkag18@gmail.com>
2024-07-26 15:25:14 +0530

    Add initial components and configuration files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import NavBar from "./components/NavBar";
+import Slider from "./components/Slider";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Slider />
+      <HeroSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+const HeroSection = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default HeroSection;
